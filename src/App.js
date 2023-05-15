e264e6f3ca28dc15d6a998fde3032a77b77c8c29
daeuk heo <gjeodnr2@gmail.com>
2023-05-15 17:39:59 +0900

    230515

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CounterBox from './components/CounterBox';
+import MemoComp from './components/MemoComp';
+
+function App() {
+  return (
+    <div className="App">
+      <CounterBox/>
+      <MemoComp />
+    </div>
+  );
+}
+
+export default App;
